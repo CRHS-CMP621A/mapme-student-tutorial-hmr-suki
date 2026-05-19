1062adcceac343db249847f87a95f6197c704a90
hmr-suki <yanan2023715@gmail.com>
2026-05-19 10:02:30 -0300

    t8

---
+++ GeoLocationAPI/script.js
@@ -0,0 +1,27 @@
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    // var map = L.map("map").setView([51.505, -0.09], 13);
+    const coords = [latitude, longitude];
+    var map = L.map("map").setView(coords, 13);
+
+    console.log(latitude, longitude);
+
+    // var map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
+    alert("Could not get position");
+  }
+);
